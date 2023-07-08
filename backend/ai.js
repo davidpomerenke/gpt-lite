9755bdcaea9645cd018fe933b9c1e0eda42a92cd
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2023-07-08 21:11:44 +0200

    Make AI route manual again

--- backend/ai.js
+++ backend/ai.js
@@ -1,4 +1,3 @@
-const { makeRoute } = require("./util");
 const { OpenAIClient } = require("@fern-api/openai");
 require("dotenv").config();
 
@@ -6,31 +5,39 @@ const client = new OpenAIClient({
   token: process.env.OPENAI_API_KEY,
 });
 
-const aiRoutes = makeRoute("/ai", async (msg) => {
-  const textChunk = await reply(msg);
-  return textChunk;
-});
+const aiRoutes = (app) => {
+  app.ws("/ai", function (ws, req) {
+    ws.on("message", (msg) => {
+      try {
+        const chatMessages = JSON.parse(msg);
+        reply(chatMessages, ws);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
+};
 
-async function reply(chatMessages, ws) {
-  await client.chat.createCompletion(
-    {
-      model: "gpt-4",
-      messages: chatMessages,
-      temperature: 0,
-      maxTokens: 2048,
-      stream: true,
-    },
+const reply = (chatMessages, ws) => {
+  const params = {
+    model: "gpt-4",
+    messages: chatMessages,
+    temperature: 0,
+    maxTokens: 2048,
+    stream: true,
+  };
+
+  client.chat.createCompletion(
+    params,
     (data) => {
       const textChunk = data.choices[0].delta.content;
-      if (textChunk) ws.send(textChunk);
+      if (textChunk) ws.send(JSON.stringify(textChunk));
     },
     {
-      onError: (error) => {
-        console.log("Received error", error);
-      },
-      onFinish: () => {},
+      onError: (e) => console.warn(e),
+      onFinish: () => console.log("finished"),
     }
   );
-}
+};
 
 module.exports = { aiRoutes };
