4d04f26927a956bf353093df369ae7d45fefeb10
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2023-07-09 18:56:22 +0200

    Change payment link

--- frontend/src/Config.elm
+++ frontend/src/Config.elm
@@ -5,9 +5,9 @@ serverUrl =
     "http://localhost:3000"
 
 
-paymentLink =
+paymentLink_ =
     "https://buy.stripe.com/test_dR65kS7xw4o7dfG9AA"
 
 
-paymentLink_ =
+paymentLink =
     "https://buy.stripe.com/4gwcMOfao2P4ego9AA"
